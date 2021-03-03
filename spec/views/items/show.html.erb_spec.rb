78d3e4621e9efdb1c579ae0ed9f9e3fd04710d95
IgorBanjac91 <igor.banjac91@gmail.com>
2021-03-03 13:41:12 +0100

    add item show page test spec

--- spec/views/items/show.html.erb_spec.rb
+++ spec/views/items/show.html.erb_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper" 
 
 RSpec.describe "items/show.html.erb", tyep: :view do 
+  
+  let(:item) { create(:item, title: "Steak", description: "Juicy", price: 23.33 ) }
 
+  before(:each) do 
+    assign(:item, item)
+    render
+  end
 
+  it "shows item information" do 
+    
+    expect(rendered).to match /Steak/
+    expect(rendered).to match /Juicy/
+    expect(rendered).to match /23.33/
+  end
 end
